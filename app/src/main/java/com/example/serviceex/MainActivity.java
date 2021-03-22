a7a7ecbc0d6ae36f8a09937a1c3834e6901544d8
hln9066@gmail.com <790799332op*>
2021-03-22 17:48:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/serviceex/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.serviceex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnStart, btnStop;
+    Intent intent; //서비스에서도 사용
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart=findViewById(R.id.btnStart);
+        btnStop=findViewById(R.id.btnStop);
+        intent=new Intent(this, MusicService.class);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(intent);
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/serviceex/MusicService.java
@@ -0,0 +1,32 @@
+package com.example.serviceex;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class MusicService extends Service {
+    MediaPlayer mp;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mp=MediaPlayer.create(this, R.raw.sky);
+        mp.setLooping(true);
+        mp.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        mp.stop();
+        super.onDestroy();
+    }
+}
