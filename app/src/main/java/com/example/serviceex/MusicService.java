ed9321003edebdcdc0a8c57ced2efd14bd14256e
hln9066@gmail.com <790799332op*>
2021-03-23 17:18:21 +0900

    Notification

--- app/src/main/java/com/example/serviceex/MusicService.java
+++ app/src/main/java/com/example/serviceex/MusicService.java
@@ -1,14 +1,29 @@
 package com.example.serviceex;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.IBinder;
+import android.widget.RemoteViews;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class MusicService extends Service {
     MediaPlayer mp;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        startForegroundService();
+    }
 
     @Nullable
     @Override
@@ -29,4 +44,55 @@ public class MusicService extends Service {
         mp.stop();
         super.onDestroy();
     }
+
+    void startForegroundService(){
+        Intent notiIntent=new Intent(this, MainActivity.class);
+        PendingIntent pIntent=PendingIntent.getActivity(this, 0, notiIntent, 0);
+        //xml과 연동하기
+        RemoteViews remoteViews=new RemoteViews(getPackageName(), R.layout.notification);
+        NotificationCompat.Builder builder;
+        if(Build.VERSION.SDK_INT>=26){
+            String channelID="musicServiceChannel";
+            //IMPORTANCE_DEFAULT>> 매니저가 알아서 우선순위를 결정함
+            NotificationChannel channel=new NotificationChannel(channelID,
+                    "음악 채널", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("채널 정의하는 곳");
+            channel.enableLights(true);
+            channel.setLightColor(Color.GREEN);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{100, 200, 100, 200});
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            ((NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE))
+                    .createNotificationChannel(channel);
+            builder=new NotificationCompat.Builder(this, channelID);
+        }else {
+            builder=new NotificationCompat.Builder(this);
+        }
+        builder.setSmallIcon(R.drawable.guitar)
+                .setContent(remoteViews)
+                .setContentIntent(pIntent);
+        startForeground(1, builder.build());
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
